c55f3a60b75d4ed741c4f388fc192e939218b4ca
GordonNY <gordonsdev@gmail.com>
2016-07-13 12:10:42 -0400

    add controllers and views

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,50 @@
 class FiguresController < ApplicationController
+	set :views, Proc.new { File.join(root, "../views/figure/") }
 
+	get '/figures/new' do 
+		erb :new
+	end
+
+	post '/figures' do 
+		figure = Figure.new(params[:figure])
+		
+		if !params[:title][:name].empty?
+			figure.titles << Title.create(name: params[:title][:name])
+		elsif !params[:landmark][:name].empty?
+			figure.landmarks << Landmark.create(name: params[:landmark][:name])
+		end
+
+		if figure.save
+			redirect "/figures/#{figure.id}"
+		else
+			puts "failed to create a figure"
+		end	
+	end
+
+	get '/figures' do 
+		@figures = Figure.all
+		erb :index
+	end
+
+	get '/figures/:id' do 
+		@figure = Figure.find(params[:id])
+		erb :show
+	end
+
+	get '/figures/:id/edit' do 
+		@figure = Figure.find(params[:id])
+		erb :edit
+	end
+
+	post '/figures/:id' do
+
+		@figure = Figure.find(params[:id])
+		@figure.update(params[:figure])
+
+		if !params[:landmark][:name].empty?
+			@figure.landmarks << Landmark.create(name: params[:landmark][:name])
+		end
+
+		redirect "/figures/#{@figure.id}"
+	end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,28 @@
 class LandmarksController < ApplicationController
+	set :views, Proc.new { File.join(root, "../views/landmarks/") }
 
+	get '/landmarks/new' do 
+		erb :new
+	end
+	post '/landmarks' do 
+		landmark = Landmark.create(params[:landmark])
+		redirect '/landmarks'
+	end
+	get '/landmarks' do
+
+		erb :index
+	end
+	get '/landmarks/:id' do 
+		@landmark = Landmark.find(params[:id])
+		erb :show
+	end
+	get '/landmarks/:id/edit' do 
+		@landmark = Landmark.find(params[:id])
+		erb :edit
+	end
+	post '/landmarks/:id' do 
+		@landmark = Landmark.find(params[:id])
+		@landmark.update(params[:landmark])
+		erb :show
+	end
 end
