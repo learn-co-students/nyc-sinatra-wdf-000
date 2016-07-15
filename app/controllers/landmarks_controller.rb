2c13b63c75b02616c7ff4ae9b466f8d308c29310
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-07-15 10:43:22 -0400

    can't find 500 issue

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,39 @@
 class LandmarksController < ApplicationController
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
 
+  post '/landmarks' do
+    landmark = Landmark.create(params[:landmark])
+    if !params[:figure][:name].empty?
+      figure = Figure.create(params[:landmark])
+      landmark.figure == figure
+      landmark.save
+    end 
+  end
+
+  get '/landmarks/new' do
+    @landmarks = Landmark.all
+    @figures = Figure.all
+    erb :'landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  post '/landmarks/:id' do
+    landmark = Landmark.find(params[:id])
+    landmark.update(params[:landmark])
+    redirect to "/landmarks/#{landmark.id}"
+  end
+  
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    @figures = Figure.all
+    @titles = Title.all
+    erb :'landmarks/edit'
+  end
 end
