64dbfc8480a1ce34f0cd6cbf43eb60e00ce4cb57
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-07-20 16:13:01 -0400

    Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+set :session_secret, "my_application_secret"
+set :views, Proc.new { File.join(root, "../views/") }
 
-  set :views, Proc.new { File.join(root, "../views/") }
-  register Sinatra::Twitter::Bootstrap::Assets
+enable :sessions
+
+  use Rack::MethodOverride
 end
