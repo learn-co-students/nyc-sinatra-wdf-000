c2408b035cf2e03ca4da69e4673c1700e7caa60a
ozPop <ozziepopovas@gmail.com>
2016-08-14 13:57:16 -0400

    add patch support, update edit, show figure views & controller
    add Rack::MethodOverride
    add form to edit figure view
    update show view to display required info
    reflect changes in controller

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -28,4 +28,26 @@ class FiguresController < ApplicationController
     erb :'/figures/show'
   end
 
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    unless params[:title][:name].empty?
+      title = Title.create(params[:title])
+      title.save
+      @figure.titles << title
+    end
+    unless params[:landmark][:name].empty?
+      landmark = Landmark.create(params[:landmark])
+      landmark.save
+      @figure.landmarks << landmark
+    end
+    @figure.save
+    redirect "/figures/#{@figure.id}"
+  end
+
 end
--- config.ru
+++ config.ru
@@ -7,6 +7,8 @@ if defined?(ActiveRecord::Migrator) && ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending run `rake db:migrate` to resolve the issue.'
 end
 
+use Rack::MethodOverride
+
 # auto-add controllers
 Dir[File.join(File.dirname(__FILE__), "app/controllers", "*.rb")].collect {|file| File.basename(file).split(".")[0] }.reject {|file| file == "application_controller" }.each do |file|
   string_class_name = file.split('_').collect { |w| w.capitalize }.join
