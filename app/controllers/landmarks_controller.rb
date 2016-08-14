8d332fde7061076386e4f7e6ec54fd1f6a5fd147
ozPop <ozziepopovas@gmail.com>
2016-08-14 15:46:54 -0400

    update landmark edit & show views, reflect in controller

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -14,4 +14,20 @@ class LandmarksController < ApplicationController
     redirect '/landmarks'
   end
 
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :"/landmarks/show"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :"/landmarks/edit"    
+  end
+
+  patch '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params[:landmark])
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
 end
