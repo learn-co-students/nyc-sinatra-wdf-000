aea7fb5362118ad7c59ab9230a47690111a56c91
ozPop <ozziepopovas@gmail.com>
2016-08-14 10:45:45 -0400

    update application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < Sinatra::Base
-
+  register Sinatra::ActiveRecordExtension
+  set :session_secret, "my_application_secret"
+  enable :sessions
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
 
