3705c9ed74cefa271c41dc2aa7398031d154ea7a
satub <satu.barnhill@gmail.com>
2016-08-18 13:59:58 -0400

    Refactores and bugfixed FigureController

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -52,46 +52,19 @@ class FiguresController < ApplicationController
     end
 
     patch "/figures/:id" do
-      figure = Figure.update(params[:id], :name => params[:figure][:name])
-      if !params[:figure][:landmark_ids].nil?
-        params[:figure][:landmark_ids].each do |id|
-          landmark = Landmark.find(id)
-          landmark.figure = figure
-          landmark.save
-        end
-      end
-      if params[:landmark][:name] != ""
+      figure = Figure.find(params[:id])
+
+      if params[:landmark][:name].present?
         landmark = Landmark.find_or_create_by(:name => params[:landmark][:name], :year_completed => params[:landmark][:year_completed])
         landmark.figure = figure
-        landmark.save
       end
-
-        # binding.pry
-      new_title_ids = params[:figure][:title_ids]
-      if !new_title_ids.nil?
-          new_title_ids.each do |id|
-          assoc = FigureTitle.where(:figure_id => figure.id, :title_id => id)
-          if assoc.empty?  #only build associations if they are not already there
-            title = Title.find(id)
-            figure.figure_titles.build(:title => title)
-          end
-        end
-      end
-
-      #remove unneeded associations:
-      assoc_old = FigureTitle.where(:figure_id => figure.id)
-      assoc_old.each do |old_assoc|
-        if !new_title_ids.include?(old_assoc.title_id)
-          FigureTitle.destroy(old_assoc.id)
-        end
-      end
-
-      if params[:title][:name] != ""
+      #
+      if params[:title][:name].present?
         title = Title.find_or_create_by(:name => params[:title][:name])
         figure.figure_titles.build(:title => title)
       end
-      figure.save
-      # binding.pry
+      figure.update(params[:figure])
+      landmark.update(:figure_id => figure.id)
       redirect "/figures/#{figure.id}"
     end
 
