d7632342ff656a16cc028babd5965fd91a290d3d
fidelscodes <fidelscodes@gmail.com>
2016-08-18 17:52:49 -0400

    Add /landmarks/new and update controller accordingly

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,11 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks/new' do
+    erb :'/landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    redirect to "/landmarks/#{@landmark.id}"
+  end
 end
