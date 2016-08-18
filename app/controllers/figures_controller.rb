5d4ee6d9a1635eb861e8f0716cff863e444329c6
fidelscodes <fidelscodes@gmail.com>
2016-08-18 15:48:02 -0400

    Pass tests for creating a new figure

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,22 @@
 class FiguresController < ApplicationController
+  get '/figures/new' do
+    erb :'/figures/new'
+  end
 
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    if !params[:title][:name].empty?
+      @title = Title.create(params[:title])
+      @figure.titles << @title
+    end
+
+    if !params[:landmark][:name].empty?
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+
+    @figure.save
+    # binding.pry
+    redirect to "/figures/#{@figure.id}"
+  end
 end
