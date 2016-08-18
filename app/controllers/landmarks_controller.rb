abf4b78da74c78a75278c265ced08df051869189
fidelscodes <fidelscodes@gmail.com>
2016-08-18 18:03:26 -0400

    Add action for showing a single landmark

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -13,4 +13,9 @@ class LandmarksController < ApplicationController
     @landmarks = Landmark.all
     erb :'/landmarks/index'
   end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by(params[:id])
+    erb :'/landmarks/show'
+  end
 end
