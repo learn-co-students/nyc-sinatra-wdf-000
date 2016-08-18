8d1c84d342cf95322bed6b47f849884ff3059cb9
fidelscodes <fidelscodes@gmail.com>
2016-08-18 17:31:14 -0400

    Complete FiguresController

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -30,7 +30,25 @@ class FiguresController < ApplicationController
   end
 
   get '/figures/:id/edit' do
-    @figure = figure.find_by(params[:id])
-    # erb :'/figures/edit'
+    @figure = Figure.find_by(params[:id])
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find_by(params[:id])
+    @figure.update(params[:figure])
+
+    if !params[:title][:name].empty?
+      @title = Title.create(params[:title])
+      @figure.titles << @title
+    end
+
+    if !params[:landmark][:name].empty?
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
   end
 end
